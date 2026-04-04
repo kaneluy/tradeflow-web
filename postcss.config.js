172bda760c68cfe0ef3aed8ec134ce82f48a8f0c
Kane Lyu <kanelyu@KanedeMacBook-Air.local>
2026-04-05 00:32:22 +0800

    fix: 修复 index.css 样式和编码

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg936\cocoartf2869
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red111\green14\blue195;\red236\green241\blue247;\red0\green0\blue0;
+}
+{\*\expandedcolortbl;;\cssrgb\c51765\c18824\c80784;\cssrgb\c94118\c95686\c97647;\cssrgb\c0\c0\c0;
+}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 export\cf0 \strokec4  \cf2 \strokec2 default\cf0 \strokec4  \{\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3   plugins: \{\cb1 \
+\cb3     tailwindcss: \{\},\cb1 \
+\cb3     autoprefixer: \{\},\cb1 \
+\cb3   \},\cb1 \
+\cb3 \}\cb1 \
+}
