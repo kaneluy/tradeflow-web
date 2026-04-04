b4413c23b32958531a07e06355bc33c697af68df
Kane Lyu <kanelyu@KanedeMacBook-Air.local>
2026-04-05 00:56:33 +0800

    fix: 使用纯文本重写配置文件彻底解决乱码

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,11 @@
-{\rtf1\ansi\ansicpg936\cocoartf2869
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red77\green80\blue85;\red236\green241\blue247;\red0\green0\blue0;
-\red111\green14\blue195;\red24\green112\blue43;}
-{\*\expandedcolortbl;;\cssrgb\c37255\c38824\c40784;\cssrgb\c94118\c95686\c97647;\cssrgb\c0\c0\c0;
-\cssrgb\c51765\c18824\c80784;\cssrgb\c9412\c50196\c21961;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 /** @type \{import('tailwindcss').Config\} */\cf0 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf5 \cb3 \strokec5 export\cf0 \strokec4  \cf5 \strokec5 default\cf0 \strokec4  \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3   content: [\cb1 \
-\cb3     \cf6 \strokec6 "./index.html"\cf0 \strokec4 ,\cb1 \
-\cb3     \cf6 \strokec6 "./src/**/*.\{js,ts,jsx,tsx\}"\cf0 \strokec4 ,\cb1 \
-\cb3   ],\cb1 \
-\cb3   theme: \{\cb1 \
-\cb3     extend: \{\},\cb1 \
-\cb3   \},\cb1 \
-\cb3   plugins: [],\cb1 \
-\cb3 \}\cb1 \
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
